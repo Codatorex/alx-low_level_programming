2c5e76e06fe21d961cfa1f0e5a152eae3fc71637
Codatorex <xpointoff@gmail.com>
2023-02-21 05:46:00 -0800

    c code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <main.h>
 
-int main(void)
-{
-	printf("_putchar\n");
+int main() {
+    // Write C code here
+    int i;
+    char function[] = "i";
+    for (i=0; i < 8; i++)
+    {
+        if (function[i]==0)
+        {
+            break;
+        }
+        else
+         {
+             putchar(function[i]);
+         }
+    }
 
-
-	return(0);
-}
+    return 0;
+}    
