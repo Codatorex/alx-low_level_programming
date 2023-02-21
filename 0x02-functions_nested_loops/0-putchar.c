1f79c0bbf12dc4c36d996d3017d29edeeb2b07d5
Codatorex <xpointoff@gmail.com>
2023-02-21 07:04:22 -0800

    c code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
+#define MAXSTRING 80
 
-int main(void) {
-    
-    int i;
-    char function[] = "_putchar";
-    for (i = 0; i < 8; i++)
-    {
-        if (function[i] == 0)
-        {
-            break;
-        }
-        else
-        {
-            _putchar(function[i]);
-        }
-    }
-
-    return 0;
+int main(void)
+{	
+	int i;
+	char function[] = "_putchar";
+	for (i = 0; i < MAXSTRING; i++)
+		{
+			if (function[i] == 0)
+			{
+			       	break;
+			}
+			else
+			{
+				_putchar(function[i]);
+			}
+		}
+		printf("\n");
+		return (0);
 }    
