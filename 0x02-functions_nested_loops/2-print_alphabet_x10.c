f42d24c275f283e5a832d1a97228144a21fb141b
Codatorex <xpointoff@gmail.com>
2023-02-21 14:42:05 -0800

    c code

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+
+{
+    int i;
+    char alpha;
+    for(i = 1; i <= 10; i++)
+    {
+    	for(alpha ='a'; alpha <= 'z'; alpha++)
+   	 {
+          _putchar(alpha);
+   	 }
+     _putchar('\n');
+    }
+}
