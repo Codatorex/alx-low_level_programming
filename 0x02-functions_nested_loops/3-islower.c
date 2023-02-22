213f7af3fa2a1c812fbd8bc9c9068361c4bf545e
Codatorex <xpointoff@gmail.com>
2023-02-21 16:43:39 -0800

    c code

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,9 @@
 
 int _islower(int c)
 {
-
-   if(c  >= 97 && c <= 122)
+   if(c >= 97 && c <= 122)
    {
-      _putchar('0');
-   }
-   else
-   {
-      _putchar('1');
+      return(1);
    }
    return(0);
 }
