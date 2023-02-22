6ae3e475f7c88d64924e85c37c0bf92c49e4f66d
Codatorex <xpointoff@gmail.com>
2023-02-22 06:45:02 -0800

    c code

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - check alphabets lowercase and uppercase.
+ * @c : the parameter that need to be checked.
+ * Return: 1 if it's an alphabet 0 if it's special character.
+ */
+int _isalpha(int c)
+{
+if (c >= 97 && c <= 122)
+{
+	return (1);
+}
+else if (c >= 65 && c <= 90)
+{
+	return (1);
+}
+return (0);
+}
