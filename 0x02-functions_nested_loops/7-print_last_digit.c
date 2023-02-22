37768339c71038f352c2d5ff9193926cb2c6c50a
Codatorex <xpointoff@gmail.com>
2023-02-22 08:32:03 -0800

    c code

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit  - returns the last digit of a decimal number.
+ * @n : parameter that must be checked.
+ * Return: d the last digit.
+ */
+int print_last_digit(int n)
+{
+int d;
+d = n % 10;
+if (d < 0)
+{
+d = d * -1;
+return (d);
+}
+else
+{
+return (d);
+}
+}
