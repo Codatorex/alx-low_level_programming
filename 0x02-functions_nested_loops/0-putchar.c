45d6ea391a419829e0d946a9054be3e0f95004c9
Codatorex <xpointoff@gmail.com>
2023-02-22 05:40:31 -0800

    c code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,25 @@
 #include "main.h"
 #define MAXSTRING 80
-/*
-* putchar prints one single character
-*
-*/
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
-{	
-	int i;
-	char function[] = "_putchar";
-	for(i = 0; i < MAXSTRING; i++)
+{
+int i;
+char function[] = "_putchar";
+for (i = 0; i < MAXSTRING; i++)
+{
+if (function[i] == 0)
 	{
-	if (function[i] == 0)
-		{
-		break;
-		}
-		else
-		{
-		_putchar(function[i]);
-		}
+	break;
 	}
-	_putchar('\n');
-	return (0);
-}    
+	else
+	{
+	_putchar(function[i]);
+	}
+}
+_putchar('\n');
+return (0);
+}
