f97e0f6fa8b65bfd650a5dc158a16695ba4f08cc
Codatorex <xpointoff@gmail.com>
2023-02-24 09:52:14 -0800

    debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
-* main - tests function that prints if integer is positive or negative
+* main - tests function that prints if integer is positive or negative.
+* positive_or_negative : check the number if it is positive or negative
 * Return: 0
 */
 int main(void)
