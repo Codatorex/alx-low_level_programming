a676babae1279a9603dd2a4d54f578b412c33d66
Codatorex <xpointoff@gmail.com>
2023-02-28 03:27:10 -0800

    c code

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-*reset_to_98 - function takes a pointer to an int as parameter and gives its  value.
-*@n : parameter that needs to be checked.
+*reset_to_98 - function takes a pointer to int as parameter &  gives its value.
+*@n: parameter that needs to be checked.
 *Return: n.
 */
 void reset_to_98(int *n)
