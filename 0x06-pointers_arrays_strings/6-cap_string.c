295ead7b4b62b08d7ad8b6d2a5595931bc059203
Codatorex <xpointoff@gmail.com>
2023-03-02 14:25:57 -0800

    c code

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,41 @@
 #include <ctype.h>
 #include <string.h>
 #include "main.h"
+
 /**
- * cap_string - function that capitalizes all words of a string.
- * @str : parameter needs to be checked.
- * Return: str.
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
 {
-int len = strlen(str);
-int cap_next = 1, i;
+	int index = 0;
 
-for (i = 0; i < len; i++)
-{
-if (isspace(str[i]) || str[i] == ',' || str[i] == ';' ||
-str[i] == '.' || str[i] == '!' || str[i] == '?' ||
-str[i] == '"' || str[i] == '(' || str[i] == ')' ||
-str[i] == '{' || str[i] == '}')
-{
-cap_next = 1;
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
+	}
+
+	return (str);
 }
-else if (cap_next)
-{
-str[i] = toupper(str[i]);
-cap_next = 0;
-}
-else
-{
-str[i] = tolower(str[i]);
-}
-}
-return (str);
-}
+
