f2bca8f657b7af598b354cdb6b158abac004f5e6
Codatorex <xpointoff@gmail.com>
2023-03-14 17:14:42 -0700

    learn malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,26 +21,16 @@
 char *create_array(unsigned int size, char c)
 		
 {
-		
-	char *str;
-		
-	unsigned int i;
-		
+char *str;
+unsigned int i;
 
-		
-	str = malloc(sizeof(char) * size);
-		
-	if (size == 0 || str == NULL)
-		
-		return (NULL);
-		
+str = malloc(sizeof(char) * size);
 
-		
-	for (i = 0; i < size; i++)
-		
-		str[i] = c;
-		
-	return (str);
-		
+if (size == 0 || str == NULL)
+return (NULL);
+
+for (i = 0; i < size; i++)
+str[i] = c;
+return (str);
 }
 
