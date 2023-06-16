19687f38234524f642031c4d9a1738e530643dfb
codatorex <xpointoff@gmail.com>
2023-06-16 08:19:33 -0700

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,28 +3,32 @@
 #include <string.h>
 #include "main.h"
 
-char *string_nconcat(char *s1, char *s2, unsigned int n) {
-    unsigned int len1, len2;
-    char *result;
+/**
+* *string_nconcat - concatenates n bytes of a string to another string
+* @s1: string to append to
+* @s2: string to concatenate from
+* @n: number of bytes from s2 to concatenate to s1
+*
+* Return: pointer to the resulting string
+**/
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-
-    len1 = strlen(s1);
-    len2 = strlen(s2);
-
-    if (n >= len2)
-        n = len2;
-
-    result = malloc(len1 + n + 1);
-    if (result == NULL)
-        return NULL;
-
-    memcpy(result, s1, len1);
-    memcpy(result + len1, s2, n);
-    result[len1 + n] = '\0';
-
-    return result;
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int len1, len2;
+char *result;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+len1 = strlen(s1);
+len2 = strlen(s2);
+if (n >= len2)
+n = len2;
+result = malloc(len1 + n + 1);
+if (result == NULL)
+return (NULL);
+memcpy(result, s1, len1);
+memcpy(result + len1, s2, n);
+result[len1 + n] = '\0';
+return (result);
 }
