e964e76e90dbdfd029dd3e58a4238466ceae11e6
codatorex <xpointoff@gmail.com>
2023-07-01 15:12:42 -0700

    linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,16 +1,18 @@
-#include <stddef.h> 
-/*for size_t*/
-#include "lists.h" 
-/*Include your linked list header file here*/
-
-size_t list_len(const list_t *h) {
-    size_t count = 0;
-    const list_t *current = h;
-
-    while (current != NULL) {
-        count++;
-        current = current->next;
-    }
-
-    return count;
+#include <stddef.h>
+#include "lists.h"
+/**
+ *list_len - returns the number of elements in a linked list
+ *@h: pointer to the list_t list
+ *Return: number of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+size_t count = 0;
+const list_t *current = h;
+while (current != NULL)
+{
+count++;
+current = current->next;
+}
+return (count);
 }
